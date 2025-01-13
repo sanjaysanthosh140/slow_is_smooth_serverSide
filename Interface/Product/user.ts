46d83f6db30760bb47bb07f1080a20340cf107e4
SanjaySanthosh_Software Developer <137829617+sanjaysanthosh140@users.noreply.github.com>
2025-01-13 13:30:48 +0530

    Update user.ts
    new

--- Interface/Product/user.ts
+++ Interface/Product/user.ts
@@ -3,7 +3,7 @@ import { deleteProd, FetchProduct, newProduct, updateProd} from "../Controls/Pro
 const express = require('express');
 const router = express.Router();
 
-router.post('/add',newProduct)
+router.post('/new',newProduct)
 router.get('/get',FetchProduct)
 router.delete('/delete/:id',deleteProd)
 router.patch('/update/:id',updateProd)
